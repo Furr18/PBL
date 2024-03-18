6830021d628c895245b8a52f3fd450ca9d190ea1
Furr18 <ghofurar18@gmail.com>
2024-03-18 19:23:37 +0700

    menambang hitung luas persegi panjang

---
+++ appy.py
@@ -0,0 +1,16 @@
+def luas_segitiga():
+    a = int(input ("masukkan alas segitiga: "))
+    t = int(input("masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("luas segitiga adalah: ", luas)
+
+luas_segitiga()
+
+
+def luas_persegi_panjang():
+    p = int(input("masukkan panjang persegi: "))
+    l = int(input("masukkan lebar persegi: "))
+    luas = p * l
+    print("luas persegi adalah: ", luas)
+
+luas_persegi_panjang()
